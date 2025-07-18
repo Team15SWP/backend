c798d767c1662a71b5d4bec6c024796d88966cea
aibek.bakirov <aibek.bakirov99@gmail.com>
2025-07-18 16:16:41 +0300

    method type corrected for function generate_task

--- internal/core/task/delivery/routes.go
+++ internal/core/task/delivery/routes.go
@@ -8,7 +8,7 @@ import (
 )
 
 func MapTaskRoutes(router *gin.Engine, h Handlers, authMiddleware gin.HandlerFunc) {
-	router.GET("/generate_task", authMiddleware, middlewares.PermissionHandler(constants.TaskEntity, constants.Get), h.GenerateTask)
+	router.POST("/generate_task", authMiddleware, middlewares.PermissionHandler(constants.TaskEntity, constants.Get), h.GenerateTask)
 	router.POST("/evaluate_code", authMiddleware, middlewares.PermissionHandler(constants.TaskEntity, constants.Create), h.EvaluateCodeForTask)
 	router.POST("/submit_code", authMiddleware, middlewares.PermissionHandler(constants.TaskEntity, constants.Create), h.EvaluateCodeForTask)
 	router.GET("/get_stats", authMiddleware, middlewares.PermissionHandler(constants.TaskEntity, constants.Get), h.GetStatistics)
